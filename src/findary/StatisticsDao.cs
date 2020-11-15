e983f5222c10d483197ebb9c9e41ab150b40872f
Daniel Hentzschel <dhentz2@gmx.de>
2020-11-15 19:20:30 +0100

    Implement StatisticsDao

---
+++ src/findary/StatisticsDao.cs
@@ -0,0 +1,25 @@
+﻿namespace Findary
+{
+    public class StatisticsDao
+    {
+        public EntityStatistics Directories { get; set; } = new EntityStatistics(nameof(Directories));
+
+        public EntityStatistics Files { get; set; } = new EntityStatistics(nameof(Files));
+
+        public class EntityStatistics
+        {
+            private string _name;
+
+            public EntityStatistics(string name)
+            {
+                _name = name;
+            }
+
+            public int Processed { get; set; }
+
+            public int Total { get; set; }
+
+            public override string ToString() => _name + ":" + Processed + " processed, " + Total + " total";
+        }
+    }
+}
