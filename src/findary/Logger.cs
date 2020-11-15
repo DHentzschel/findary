caee7e33e805262d6aa54a8e564923ce549898e2
Daniel Hentzschel <dhentz2@gmx.de>
2020-11-15 20:15:07 +0100

    Move print functions to class Logger

---
+++ src/findary/Logger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Findary
+{
+    public class Logger
+    {
+        private readonly Options _options;
+
+        public Logger(Options options)
+        {
+            _options = options;
+        }
+
+        public void PrintTimeElapsed(string task, long milliseconds)
+        {
+            if (!_options.MeasureTime)
+            {
+                return;
+            }
+            var seconds = (float)milliseconds / 1000;
+            Console.WriteLine("Time elapsed " + task + ": " + seconds + "s");
+        }
+
+        public void PrintVerbosely(string message, bool isError = false)
+        {
+            if (!_options.Verbose)
+            {
+                return;
+            }
+            var textWriter = isError ? Console.Error : Console.Out;
+            textWriter.WriteLine(message);
+        }
+    }
+}
