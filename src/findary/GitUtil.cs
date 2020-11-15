a26a420f0c81a12b4a6986cd9ce66ff9061329bc
Daniel Hentzschel <dhentz2@gmx.de>
2020-11-15 20:17:52 +0100

    Add ctor with Options arg to GitUtil

--- src/findary/GitUtil.cs
+++ src/findary/GitUtil.cs
@@ -4,6 +4,13 @@ namespace Findary
 {
     public class GitUtil
     {
+        private readonly Options _options;
+
+        public GitUtil(Options options)
+        {
+            _options = options;
+        }
+
         public static string GetGitFilename() => "git";
 
         public static string GetGitLfsFilename() => GetGitFilename() + (!OperatingSystem.IsWindows() ? "-lfs" : string.Empty);
