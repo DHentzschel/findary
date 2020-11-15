c2f830664e58bc4c9b8350ac842cbedf83af229d
Daniel Hentzschel <dhentz2@gmx.de>
2020-11-15 21:35:53 +0100

    Rename param to deltaMillis

--- src/findary/Logger.cs
+++ src/findary/Logger.cs
@@ -11,13 +11,13 @@ namespace Findary
             _options = options;
         }
 
-        public void PrintTimeElapsed(string task, long milliseconds)
+        public void PrintTimeElapsed(string task, long deltaMillis)
         {
             if (!_options.MeasureTime)
             {
                 return;
             }
-            var seconds = milliseconds * 0.001F;
+            var seconds = deltaMillis * 0.001F;
             Console.WriteLine("Time elapsed " + task + ": " + seconds + "s");
         }
 
