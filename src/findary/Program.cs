d381efaff386e3af26814656d8e789de0919a34d
Daniel Hentzschel <dhentz2@gmx.de>
2020-11-16 17:09:31 +0100

    Pass parameter to new Findary instance

--- src/findary/Program.cs
+++ src/findary/Program.cs
@@ -5,6 +5,6 @@ namespace Findary
     public static class Program
     {
         public static void Main(string[] args)
-            => Parser.Default.ParseArguments<Options>(args).WithParsed(p => new Findary().Run(p));
+            => Parser.Default.ParseArguments<Options>(args).WithParsed(p => new Findary(p).Run());
     }
 }
