9b35554a38b0b9c683c0eb37b5c528a947083590
Daniel Hentzschel <dhentz2@gmx.de>
2020-12-03 19:43:11 +0100

    Implement IService

---
+++ src/findary/Service/IService.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+
+namespace Findary.Service
+{
+    public interface IService
+    {
+        public Stopwatch Stopwatch { get; set; }
+
+        void Run();
+
+        void PrintTimeSpent();
+    }
+}
