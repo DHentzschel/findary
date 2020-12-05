aba176180a223e8d0583f8a92ee3f8a3dd55e61f
Daniel Hentzschel <dhentz2@gmx.de>
2020-12-05 04:11:02 +0100

    Make process a parameter instead of member

--- src/findary/GitUtil.cs
+++ src/findary/GitUtil.cs
@@ -17,18 +17,18 @@ namespace Findary
         private readonly Options _options;
 
         private readonly IFileSystem _fileSystem;
-        private readonly IProcess _process;
+        private IProcess _process;
         private readonly IOperatingSystem _operatingSystem;
-        private readonly bool _isGitAvailable;
+        private bool _isGitAvailable;
         private bool _isFirstCall = true;
 
-        public GitUtil(Options options, IFileSystem fileSystem = null, IProcess process = null, IOperatingSystem operatingSystem = null)
+        public GitUtil(Options options, IFileSystem fileSystem = null, /*IProcess process = null,*/ IOperatingSystem operatingSystem = null)
         {
             _options = options;
             _fileSystem = fileSystem ?? new FileSystem();
-            _process = process ?? new ProcessWrapper();
+            //_process = process ?? new ProcessWrapper();
             _operatingSystem = operatingSystem ?? new OperatingSystemWrapper();
-            _isGitAvailable = IsGitAvailable();
+            //_isGitAvailable = IsGitAvailable();
         }
 
         public string GetGitFilename() => "git" + GetPlatformSpecific(".exe", string.Empty);
@@ -87,9 +87,14 @@ namespace Findary
             return result;
         }
 
-        public void TrackGlobs(bool isExtension, List<string> globs, StatisticsDao statistics, int commandLength)
+        public void TrackGlobs(bool isExtension, List<string> globs, StatisticsDao statistics, int commandPrefixLength, IProcess process)
         {
-            if (!_options.Track || !_isGitAvailable || !InitGitLfs())
+            if (_process != process)
+            {
+                _process = process;
+                _isGitAvailable = IsGitAvailable(process);
+            }
+            if (!_options.Track || !_isGitAvailable || !InitGitLfs(process))
             {
                 if (_options.Track && _isFirstCall)
                 {
@@ -161,9 +166,10 @@ namespace Findary
         private List<string> GetGitIgnoreLines()
             => !_options.IgnoreFiles ? new List<string>() : GetFileLines(_options.Directory, ".gitignore");
 
-        private string GetNewProcessOutput(string filename, string arguments)
+        private string GetNewProcessOutput(string filename, string arguments, IProcess process = null)
         {
-            _process.StartInfo = new ProcessStartInfo
+            process ??= new ProcessWrapper();
+            var startInfo = new ProcessStartInfo
             {
                 FileName = filename,
                 Arguments = arguments,
@@ -171,10 +177,10 @@ namespace Findary
                 RedirectStandardOutput = true,
                 CreateNoWindow = true
             };
-
+            process.StartInfo = startInfo;
             try
             {
-                _process.Start();
+                process.Start();
             }
             catch (Exception e)
             {
@@ -182,13 +188,12 @@ namespace Findary
                 return null;
             }
 
-
             string output = null;
-            while (!_process.StandardOutput.EndOfStream)
+            while (!process.StandardOutput.EndOfStream)
             {
                 try
                 {
-                    output += (output == null ? string.Empty : "\n") + _process.StandardOutput.ReadLine();
+                    output += (output == null ? string.Empty : "\n") + process.StandardOutput.ReadLine();
                 }
                 catch (Exception e)
                 {
@@ -196,33 +201,35 @@ namespace Findary
                 }
             }
 
-            if (_process.ExitCode != 2)
+            var exitCode = process.ExitCode;
+            process.Close();
+            if (exitCode != 2)
             {
                 return output;
             }
-            _logger.Error("Access is probably denied. Exit code " + _process.ExitCode + " (try again with admin privileges)");
+            _logger.Error("Access is probably denied. Exit code " + process.ExitCode + " (try again with admin privileges)");
             return null;
         }
 
-        private bool InitGitLfs()
+        private bool InitGitLfs(IProcess process)
         {
-            var output = GetNewProcessOutput(GetGitLfsFilename(), GetGitLfsArguments("install", true));
+            var output = GetNewProcessOutput(GetGitLfsFilename(), GetGitLfsArguments("install", true), process);
             return output?.EndsWith("Git LFS initialized.") == true;
         }
 
-        private bool IsGitAvailable()
+        private bool IsGitAvailable(IProcess process)
         {
             const string arguments = "version";
-            return IsGitInstalled(arguments) && IsGitLfsInstalled(arguments);
+            return IsGitInstalled(arguments, process) && IsGitLfsInstalled(arguments, process);
         }
 
-        private bool IsGitInstalled(string arguments) => IsInstalled(GetGitFilename(), arguments, "git version");
+        private bool IsGitInstalled(string arguments, IProcess process) => IsInstalled(GetGitFilename(), arguments, "git version", process);
 
-        private bool IsGitLfsInstalled(string arguments) => IsInstalled(GetGitLfsFilename(), GetGitLfsArguments(arguments), "git-lfs/");
+        private bool IsGitLfsInstalled(string arguments, IProcess process) => IsInstalled(GetGitLfsFilename(), GetGitLfsArguments(arguments), "git-lfs/", process);
 
-        private bool IsInstalled(string filename, string arguments, string outputPrefix)
+        private bool IsInstalled(string filename, string arguments, string outputPrefix, IProcess process)
         {
-            var output = GetNewProcessOutput(filename, arguments);
+            var output = GetNewProcessOutput(filename, arguments, process);
             if (output?.StartsWith(outputPrefix) == true)
             {
                 return true;
