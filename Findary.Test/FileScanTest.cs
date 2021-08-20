c45fd2b63e6c8e5a8a3ceb6772b78beb6744b715
Daniel Hentzschel <dhentz2@gmx.de>
2021-08-20 20:15:54 +0200

    Implement FileScanTest

---
+++ Findary.Test/FileScanTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Findary.Test
+{
+    public class FileScanTest
+    {
+        [Test]
+        public void TestIsFileBinary()
+        {
+            var input = new byte[] { 0x1, 0x0, 0, 2 };
+            var memoryStream = new MemoryStream(input);
+            var fileScan = new FileScan.FileScan();
+            Assert.IsTrue(fileScan.IsFileBinary(memoryStream));
+        }
+
+        [Test]
+        public void TestIsFileBinaryUtf()
+        {
+            var input = new byte[] { 0x48, 0x0, 0x64, 0x0, 0x6c, 0x0, 0x6c, 0x0, 0x6f, 0x0 };
+            var memoryStream = new MemoryStream(input);
+            var fileScan = new FileScan.FileScan();
+            Assert.IsTrue(fileScan.IsFileBinary(memoryStream));
+        }
+
+        [Test]
+        public void TestIsFileBinaryText()
+        {
+            var input = Encoding.ASCII.GetBytes("hello");
+            var memoryStream = new MemoryStream(input);
+            var fileScan = new FileScan.FileScan();
+            Assert.IsFalse(fileScan.IsFileBinary(memoryStream));
+        }
+
+        [Test]
+        public void TestIsFileBinaryNonPrintable()
+        {
+            byte[] ConvertIntToByte(IEnumerable<int> src) => src.Select(t => (byte)t).ToArray();
+            var input = ConvertIntToByte(Enumerable.Range(1, 31).ToArray());
+            var memoryStream = new MemoryStream(input);
+            var fileScan = new FileScan.FileScan();
+            Assert.IsFalse(fileScan.IsFileBinary(memoryStream));
+        }
+
+        [Test]
+        public void TestIsFileBinaryEmpty()
+        {
+            var input = Array.Empty<byte>();
+            var memoryStream = new MemoryStream(input);
+            var fileScan = new FileScan.FileScan();
+            Assert.IsFalse(fileScan.IsFileBinary(memoryStream));
+        }
+    }
+}
